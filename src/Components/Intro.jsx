2b2db6efe8c0db67b68ce6dec64aa0e132c40cd7
Sir Harunzy <Harunzy55@gmail.com>
2025-07-18 16:06:36 +0000

    add an intro component

---
+++ src/Components/Intro.jsx
@@ -0,0 +1,34 @@
+export default function Intro() {
+  return (
+    <div>
+      <div className="container">
+        <p>
+          Available for <strong>Opportunities</strong>
+        </p>
+        <h1>Fullstack Developer</h1>
+        <p>
+          A passionate and results-oriented Software Engineer based in Ghana, I
+          specialize in crafting dynamic and user-friendly web applications. My
+          expertise lies in leveraging modern technologies to build scalable and
+          efficient solutions. I bring a strong understanding of front-end
+          development with React and responsive design using Bootstrap. On the
+          backend, I am proficient in building robust and scalable APIs with
+          Node.js and managing data effectively with MongoDB. I am eager to
+          contribute my skills to innovative projects and collaborate with
+          forward-thinking teams. Explore my portfolio to see examples of my
+          work.
+        </p>
+        <div>
+          <button className="btn btn-primary">View My Work</button>
+          <button className="btn btn-outline-warning ">Let's Connect</button>
+          <p>
+            <i class="fa-brands fa-square-github"></i>
+            <i class="fa-brands fa-linkedin"></i>
+            <i class="fa-brands fa-dev"></i>
+          </p>
+        </div>
+        <img src="./images/Profile.png" alt="Profile" />
+      </div>
+    </div>
+  );
+}
